fd10856dda41a3ef0e30fe94693cd4f20b7e39a6
Kc <Kistine@users.noreply.github.com>
2026-04-01 08:43:10 -0400

    Create LI-Ease-of-Business.m
    Add L&I ease of business data

---
+++ _datasets/LI-Ease-of-Business.m
@@ -0,0 +1,37 @@
+---
+area_of_interest: null
+category:
+- Economy
+- Planning / Zoning
+license: City of Philadelphia License
+maintainer: ligisteam@phila.gov
+maintainer_email: LIGISTEAM@phila.gov
+maintainer_link: null
+maintainer_phone: null
+notes: "This table provides review times for L&I plan reviews. This does not reflect total permit review times,
+  only L&I-specific review times. Total review times are affected by factors outside of L&I's control. Note that
+  review times are delayed on permits with variances, so records where variance = 'Y' show longer review periods."
+metadata_modified: null
+modified: R/P1D
+organization: City of Philadelphia
+resources:
+- description: ''
+  format: CSV
+  name: L&I Business Licenses (CSV)
+  url: https://hub.arcgis.com/api/v3/datasets/dbe11bf60b2e4c16916abf43042f7e2d_0/downloads/data?format=csv&spatialRefId=3857&where=1%3D1
+- description: ''
+  format: API
+  name: L&I Business Licenses (API)
+  url: https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/ease_of_business/FeatureServer/0/query?outFields=*&where=1%3D1
+- description: ''
+  format: HTML
+  name: L&I Business Licenses (Metadata)
+  url: https://metadata.phila.gov/#home/datasetdetails/69bd68334f85501614f78e52/representationdetails/69bd68334f85501614f78e69/
+schema: philadelphia
+source: ''
+tags:
+- Department of Licenses and Inspections
+time_period: null
+title: Licenses and Inspections Ease of Business
+usage: null
+---
