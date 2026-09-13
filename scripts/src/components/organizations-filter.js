69e0d3ca7de693286c55db000ccee7f0712df851
BryanQuigley <code@bryanquigley.com>
2026-09-13 07:29:20 -0700

    Sept7 2026- drops lodash (saves about half of the javascript, updates others (#756)
    * Add sitemap.xml for better searchability for search engines, etc
    
    Replaces ODP specific: https://github.com/opendataphilly/opendataphilly-jkan/pull/726
    
    Also upgrade Gems while here.
    
    * Move boostrap to head from build, only load other javascript on key pages
    
    I noticed the css and js of bootstrap are out of sync - so I wanted them to just be loaded
    in the same way.
    
    I also found (or AI helped me) a path to migrate most of things to native Javascript
    but it was way too many changes to do in one go (https://github.com/opendataphilly/opendataphilly-jkan/pull/725)
    This is a part of that (the easy part) just broken out first.
    
    Also drop the Show more option which AFAICT is broken.
    
    * more ruby upgrades
    
    * Drop lodash dependency in favor of native JS
    
    Replace lodash usage in the scripts bundle with native Array/Object
    methods (filter, flatMap, reduce-based groupBy, sort, spread, ??),
    keeping the same behavior verified against lodash output. Removes
    ~70 kB (24 kB gzipped) from the served bundle.
    
    * Bump browserslist from 4.28.2 to 4.28.9
    
    Bumps [browserslist](https://github.com/browserslist/browserslist) from 4.28.2 to 4.28.9.
    - [Release notes](https://github.com/browserslist/browserslist/releases)
    - [Changelog](https://github.com/browserslist/browserslist/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/browserslist/browserslist/compare/4.28.2...4.28.9)
    
    ---
    updated-dependencies:
    - dependency-name: browserslist
      dependency-version: 4.28.9
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix up js bundle
    
    not sure why this is needed, but may cleanup more.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Bryan Quigley <code@bryanquigley>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/src/components/organizations-filter.js
+++ scripts/src/components/organizations-filter.js
@@ -1,35 +1,35 @@
 import $ from 'jquery'
-import {chain, pick, omit, filter, defaults} from 'lodash'
 
 import TmplListGroupItem from '../templates/list-group-item'
-import {setContent, slugify, createDatasetFilters, collapseListGroup} from '../util'
+import {setContent, slugify, createDatasetFilters, groupBy} from '../util'
 
 export default class {
   constructor (opts) {
     const organizations = this._organizationsWithCount(opts.datasets, opts.params)
     const organizationsMarkup = organizations.map(TmplListGroupItem)
     setContent(opts.el, organizationsMarkup)
-    collapseListGroup(opts.el)
   }
 
   _organizationsWithCount (datasets, params) {
-    return chain(datasets)
-      .groupBy('organization')
-      .map(function (datasetsInOrg, organization) {
-        const filters = createDatasetFilters(pick(params, ['category']))
-        const filteredDatasets = filter(datasetsInOrg, filters)
-        const orgSlug = slugify(organization)
-        const selected = params.organization && params.organization === orgSlug
-        const itemParams = selected ? omit(params, 'organization') : defaults({organization: orgSlug}, params)
-        return {
-          title: organization,
-          url: '?' + $.param(itemParams),
-          count: filteredDatasets.length,
-          unfilteredCount: datasetsInOrg.length,
-          selected: selected
-        }
-      })
-      .orderBy('unfilteredCount', 'desc')
-      .value()
+    const organizationsBySlug = groupBy(datasets, 'organization')
+    return Object.keys(organizationsBySlug).map(function (organization) {
+      const datasetsInOrg = organizationsBySlug[organization]
+      const filters = createDatasetFilters({category: params.category})
+      const filteredDatasets = datasetsInOrg.filter(filters)
+      const orgSlug = slugify(organization)
+      const selected = params.organization && params.organization === orgSlug
+      const itemParams = selected ? omitKey(params, 'organization') : {...params, organization: orgSlug}
+      return {
+        title: organization,
+        url: '?' + $.param(itemParams),
+        count: filteredDatasets.length,
+        unfilteredCount: datasetsInOrg.length,
+        selected: selected
+      }
+    }).sort((a, b) => b.unfilteredCount - a.unfilteredCount)
   }
+}
+
+function omitKey (obj, key) {
+  return Object.fromEntries(Object.entries(obj).filter(([entryKey]) => entryKey !== key))
 }
--- scripts/src/util.js
+++ scripts/src/util.js
@@ -21,7 +21,18 @@ export function slugify (text) {
     .replace(/^\-|\-$/i, '')        // Remove leading/trailing hyphen
 }
 
-// Given an object of filters to use, returns a function to be used by _.filter()
+// Groups an array by a given key or iteratee function
+export function groupBy (items, iteratee) {
+  const keyFn = typeof iteratee === 'function' ? iteratee : (item) => item[iteratee]
+  return items.reduce((result, item) => {
+    const key = keyFn(item)
+    result[key] = result[key] || []
+    result[key].push(item)
+    return result
+  }, {})
+}
+
+// Given an object of filters to use, returns a function to be used by filter()
 export function createDatasetFilters (filters) {
   return function (dataset) {
     const conditions = []
@@ -35,22 +46,3 @@ export function createDatasetFilters (filters) {
   }
 }
 
-// Collapses a bootstrap list-group to only show a few items by default
-// Number of items to show can be specified in [data-show] attribute or passed as param
-export function collapseListGroup (container, show) {
-  if (!show) show = container.data('show') || 5
-
-  const itemsToHide = $('.list-group-item:gt(' + (show - 1) + '):not(.active)', container)
-  if (itemsToHide.length) {
-    itemsToHide.hide()
-
-    const showMoreButton = $('<a href="#" class="list-group-item">Show ' + itemsToHide.length + ' more...</a>')
-    showMoreButton.on('click', function (e) {
-      itemsToHide.show()
-      $(this).off('click')
-      $(this).remove()
-      e.preventDefault()
-    })
-    container.append(showMoreButton)
-  }
-}
